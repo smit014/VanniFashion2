f93cec56b44ea26086c046fed4a15a33ba669a6c
sjmal_0004 <sjmalaviya41@gmail.com>
2023-08-15 19:30:20 +0530

    thired

--- app/src/main/java/com/example/vannifashion/SigninActivity.java
+++ app/src/main/java/com/example/vannifashion/SigninActivity.java
@@ -1,14 +1,34 @@
 package com.example.vannifashion;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SigninActivity extends AppCompatActivity {
-
+    EditText susername;
+    EditText pass;
+    AppCompatButton b_signin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+        susername =findViewById(R.id.username);
+        pass = findViewById(R.id.password);
+        b_signin =(AppCompatButton) findViewById(R.id.bt_signin);
+        b_signin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openActivity();
+            }
+        });
+    }
+    public void openActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/vannifashion/SignupActivity.java
+++ app/src/main/java/com/example/vannifashion/SignupActivity.java
@@ -1,14 +1,39 @@
 package com.example.vannifashion;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SignupActivity extends AppCompatActivity {
+    private AppCompatButton button;
+    EditText username;
+    EditText email;
+    EditText fullname;
+    EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        username =findViewById(R.id.signupusername);
+        email =findViewById(R.id.email);
+        fullname =findViewById(R.id.name);
+        password =findViewById(R.id.signuppassword);
+        button =(AppCompatButton) findViewById(R.id.b_signup);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                openActivity2();
+            }
+        });
+    }
+    public void openActivity2(){
+        Intent intent = new Intent(this, SigninActivity.class);
+        startActivity(intent);
     }
 }
